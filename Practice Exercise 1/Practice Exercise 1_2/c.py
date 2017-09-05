64f243cbee8fa41e0682776379a1c053b78b300c
Joost Lekkerkerker <joostlek@outlook.com>
2017-09-05 12:13:00 +0200

    Practice Exercise 1_2 finished + Practice Exercise 1_2c + Practice Exercise 1_2d

---
+++ Practice Exercise 1/Practice Exercise 1_2/c.py
@@ -0,0 +1,7 @@
+str1 = 'Antidisestablishmentarianism'
+str2 = 'Honorificabilitudinitatibus'
+if str1 > str2:
+    print(True)
+else:
+    print(False)
+"False"
---
+++ Practice Exercise 1/Practice Exercise 1_2/d.py
@@ -0,0 +1,6 @@
+names = ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein']
+names.sort()
+print('eerste: ' + names[0])
+print('laatste: ' + names[len(names) - 1])
+""""eerste: Bartok
+    laatste: Buxtehude"""
